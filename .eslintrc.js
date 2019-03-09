133c2c47a366648f305a4a0c7f4d89d527326d7d
Fajkowsky <dawid.fajkowski@gmail.com>
2019-03-09 14:21:11 +0100

    refactor initial project, add eslint extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,17 @@ module.exports = {
     node: true,
   },
   extends: [
+    'plugin:vue/base',
     'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/recommended',
     '@vue/airbnb',
+    'eslint:recommended',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
   },
   parserOptions: {
     parser: 'babel-eslint',
