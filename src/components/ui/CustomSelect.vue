daec1fe984c465c3508ba5eeeb037b466cdadc11
Fajkowsky <dawid.fajkowski@gmail.com>
2019-03-11 20:10:14 +0100

    create custom select component, pass props

---
+++ src/components/ui/CustomSelect.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="custom-select">
+    <ul>
+      <li
+        v-for="option in options"
+        :key="option.id"
+        :value="option.id"
+      >
+        {{ $t(option.text) }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomSelect',
+  props: {
+    options: {
+      type: Array,
+      required: true,
+      validator(value) {
+        return value.every(item => 'id' in item && 'text' in item);
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  #custom-select {
+    color: black;
+  }
+
+</style>
